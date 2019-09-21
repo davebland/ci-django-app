52dfa9a605a25aa2b7b2af4083abb3193e2854c4
Ubuntu <ubuntu@ip-172-31-6-48.eu-west-1.compute.internal>
2019-09-21 09:45:46 +0000

    corrected db connection string

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 #     }
 # }
 
-DATABASES = {'default' : {os.environ.get('DATABASE_URL')}}
+DATABASES = {'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
